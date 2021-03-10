21bfec4807d4d0fef4de8df48a799c24ae0272e6
Isaac <cavalcante.isaac1@gmail.com>
2021-03-10 00:53:51 -0300

    Initial Commit

---
+++ DiceRoll/DiceRollApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollApp.swift
+//  DiceRoll
+//
+//  Created by Isaac Cavalcante on 10/03/21.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
